7238f73bcd835dc0299fd26b7ac0d41729def33a
Renato Groffe <renato.groffe@yahoo.com.br>
2021-01-09 16:30:47 -0300

    Inclusao de arredondamento por casas decimais

--- Groffe.Distancias/ConversorDistancias.cs
+++ Groffe.Distancias/ConversorDistancias.cs
@@ -4,7 +4,7 @@ namespace Groffe.Distancias
 {
     public static class ConversorDistancias
     {
-        public static double MilhasParaKm(double milhas) =>
-            Math.Round(Convert.ToDouble(milhas) * 1.609, 3);
+        public static double MilhasParaKm(double milhas, int casasArredondamento = 3) =>
+            Math.Round(Convert.ToDouble(milhas) * 1.609, casasArredondamento);
     }
 }
