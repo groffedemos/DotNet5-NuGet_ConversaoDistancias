d90c88154c68d32f6f5ff9db08795e761d776a55
Renato Groffe <renato.groffe@yahoo.com.br>
2021-03-30 15:13:14 -0300

    Update ConversorDistancias.cs

--- Groffe.Distancias/ConversorDistancias.cs
+++ Groffe.Distancias/ConversorDistancias.cs
@@ -4,7 +4,9 @@ namespace Groffe.Distancias
 {
     public static class ConversorDistancias
     {
-        public static double MilhasParaKm(double milhas, int casasArredondamento = 3) =>
-            Math.Round(Convert.ToDouble(milhas) * 1.609, casasArredondamento);
+        public static double MilhasParaKm(double milhas) =>
+            Math.Round(Convert.ToDouble(milhas) * 1.609, 3);        
+        //public static double MilhasParaKm(double milhas, int casasArredondamento = 3) =>
+        //    Math.Round(Convert.ToDouble(milhas) * 1.609, casasArredondamento);
     }
 }
